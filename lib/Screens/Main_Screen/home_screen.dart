fce038b172bc1fbc162b3678344d92c98057a300
gulnwaot55 <aotzatingtong5@gmail.com>
2023-02-21 11:12:24 +0700

    Update Bottom

--- lib/Screens/Main_Screen/home_screen.dart
+++ lib/Screens/Main_Screen/home_screen.dart
@@ -1,11 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:netflix/Screens/Main_Screen/login_screen.dart';
-import 'package:netflix/Screens/Multi_Screen/navbar_screen.dart';
 
 import '../../Controllers/app_controller.dart';
 import '../../Widgets/app_button.dart';
+import '../Multi_Screen/navbar_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,11 +17,11 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        bottomNavigationBar: NavBarScreen(),
+        bottomNavigationBar: BottomNavbarScreen(),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Home Screen"),
+            const Text("Home Screen"),
             appButtonLogOut(context, true, () {
               AppController.setlogOut();
             }),
--- lib/Screens/Multi_Screen/splash_screen.dart
+++ lib/Screens/Multi_Screen/splash_screen.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:netflix/Controllers/app_controller.dart';
